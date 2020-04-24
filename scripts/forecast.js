649645da02cabc174be24502f4dad3c006245d27
azmeelkoya <hello@azmeelkoya.com>
2020-04-24 16:34:12 +0530

    added necessary files and did phase one

---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const key = 'i9EZcxd6flkC4SSdROZzQTG3UyQGYH5d';
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+const getCondition = async (locationKey) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationKey}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// getCity('manchester')
+//     .then( data => console.log(data.Key))
+//     .catch( err => console.log(err));
+
+getCity('calicut').then(data => {
+    return getCondition(data.Key);
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err));
